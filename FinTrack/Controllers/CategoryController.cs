b5def66e7c7754d98aefa8ed9eefc5b60f309776
Luiz Otávio <luizotaviosilva492@gmail.com>
2025-09-16 23:24:45 -0300

    feat: adding category endpoints

--- FinTrack/Controllers/CategoryController.cs
+++ FinTrack/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using FinTrack.Application.Services.Interfaces;
+﻿using Fintrack.Contracts.DTOs.Category;
+using FinTrack.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -38,12 +39,30 @@ public class CategoryController : ControllerBase
     {
         try
         {
-            var category = _categoryService.GetCategoryByIdAsync(id);
+            var category = await _categoryService.GetCategoryByIdAsync(id);
 
             if (category == null)
                 return StatusCode((int)HttpStatusCode.NotFound, "Nenhuma categoria encontrada para o id inserido.");
 
             return Ok(category);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, $"Erro interno do servidor: {ex.Message}");
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCategory(CategoryCreateDto categoryCreateDto)
+    {
+        try
+        {
+            var result = await _categoryService.AddCategoryAsync(categoryCreateDto);
+
+            if (result == null)
+                return StatusCode((int) HttpStatusCode.BadRequest, "Não foi possível criar a categoria. Verifique os dados enviados.");
+
+            return StatusCode((int)HttpStatusCode.Created, "Categoria criada com sucesso!");
         }
         catch (Exception ex)
         {
